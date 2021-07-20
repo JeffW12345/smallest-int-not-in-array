798a1bb88f045f438cb8522f5437495b09fc8bd5
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-07-20 14:45:27 +0100

    Update Solution.cs
    More concise solution

--- Solution.cs
+++ Solution.cs
@@ -5,30 +5,15 @@ namespace CodilityChallenges
 
     class Solution
     {
-        // Finds the smallest int not to occur in the array
+        // Finds the smallest positive int to occur in the array
         public static int GetSmallestInt(int[] A)
         {
-            HashSet<int> asSet = new HashSet<int>(A);
-            int minPositiveInList = 0;
-            foreach (int num in asSet)
-            {
-                if (num > 0)
-                {
-                    if(num < minPositiveInList && minPositiveInList > 0)
-                    {
-                        minPositiveInList = num;
-                    }
-                    if(minPositiveInList == 0)
-                    {
-                        minPositiveInList = num;
-                    }
-                }
-            }
-            if (minPositiveInList != 1)
+            HashSet<int> asSet = new HashSet<int>(A); // Convert to set to avoid repeating values
+            if (!asSet.Contains(1))
             {
                 return 1;
             }
-            for(int i = 1; i < 1000000; i++)
+            for (int i = 2; i < 1000000; i++)
             {
                 if (!asSet.Contains(i))
                 {
@@ -40,7 +25,6 @@ namespace CodilityChallenges
 
         public static void Main(string[] args)
         {
-
             int[] arr = { 1, 3, 4, 3, 2 };
             Console.WriteLine(GetSmallestInt(arr));
         }
